22eb8494956d7ec79d5868401133ad3c89db5271
budalla-kk <thomas__wu@outlook.com>
2021-09-22 11:42:20 +0800

    Gtest 和 Google-benchmark的测试，已经基本完成，问题包括自动添加gtest-g_benchmark libs 未能实现自动化，两项测试目录不统一。

---
+++ factoriallib/falgo.h
@@ -0,0 +1,15 @@
+//
+// Created by thomas on 2021/9/21.
+//
+
+#ifndef GTEST_DEMO_FALGO_H
+#define GTEST_DEMO_FALGO_H
+
+#define ll long long
+
+namespace falgo {
+    ll simple_recursion(ll n);
+    ll fact_naive(ll n);
+}
+
+#endif // FACTORIALLIB_FALGO_H
---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Creator : Thomas Wu
+// Created by thomas on 2021/9/21.
+// Use : Simple example of factorial algorithm.
+
+#include <iostream>
+#include "falgo.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    std::cout << "Factorial of 10 is " << falgo::fact_naive(10) << std::endl;
+
+    return 0;
+}
